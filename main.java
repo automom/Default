3048d110fdad4875a87f17c3eb46b747520e4eb4
jonathan <painfulshizxd@aol.com>
2016-03-05 13:59:01 -0500

    touching up main now

--- main.java
+++ main.java
@@ -1,3 +1,5 @@
-public static void main(String[] args){
-	System.out.println("it feels weird doesn't it?");
+public class main{
+	public static void main(String[] args){
+		System.out.println("it feels weird doesn't it?");
+	}
 }
